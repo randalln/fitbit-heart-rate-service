6b59bcc1842e6f706761bbaaa171b815961098d2
Randall Norviel <randallndev@gmail.com>
2025-09-17 18:17:37 -0400

    Release 0.8.0

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -52,8 +52,8 @@ android {
         applicationId = "org.noblecow.hrservice"
         minSdk = 28
         targetSdk = 36
-        versionCode = 13
-        versionName = "0.7.0"
+        versionCode = 14
+        versionName = "0.8.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildFeatures {
